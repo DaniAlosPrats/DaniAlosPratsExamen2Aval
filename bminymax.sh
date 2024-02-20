2d29df1b96da5a5ee9af493765820dc732fa1d6a
Dani Alos <alospratsdani@gmail.com>
2024-02-20 10:23:44 +0100

    resolviendo

--- bminymax.sh
+++ bminymax.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-
-
-cosumo_minimo=""
-cosumo_maximo=""
-mayor=0
-minimo=999999999
+max=""
+min=""
+cosumo_minimo=999999999
+cosumo_maximo=0
 
 while IFS= read  -r line; do
-cosumo=$(echo $line | awk '{print $4}')
-   if [[ $consumo =~ ^[0-9]+$ ]]; then
-        if [ "$consumo" -gt "$consumo_maximo" ]; then
-            consumo_maximo=$consumo_maximo
-            cosumo_maximo="$line"
-        fi
-    fi
-done < "consumo.txt"
-
-while IFS= read -r linea; do
-    consumo=$(echo "$linea" | awk '{print $4}')
+    consumo=$(echo $line | awk '{print $4}')
     if [[ $consumo =~ ^[0-9]+$ ]]; then
+        if [ "$consumo" -gt "$cosumo_maximo" ]; then
+            consumo_maximo=$consumo
+            max="$line"
+        fi
         if [ "$consumo" -lt "$cosumo_minimo" ]; then
-            minimo=$minimo
-            cosumo_minimo="$linea"
+            cosumo_minimo=$consumo
+            min="$line"
         fi
     fi
-done < "consumo.txt"
+done < consumo.txt
 
-
-echo "El consumo minimo:"
-echo "$consumo_minimo"
-
-echo "El consumo maximo:"
-echo "$cosumo_maximo"
+echo "Maximo : $max"
+echo "Minimo : $min"
