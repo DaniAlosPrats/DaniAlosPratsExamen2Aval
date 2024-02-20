311e35d64ca3abf5d278ed825a06e9c3a5eb32bf
Dani Alos <alospratsdani@gmail.com>
2024-02-20 08:55:25 +0100

    mejorando atotal.sh

--- atotal.sh
+++ atotal.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-echo "Di una ciudad"
+
+echo "Di una ciudad:"
 read ciudad
 
-if ()
+suma=0
+
+while read line; do
+  nombre_ciudad=$(echo $line | awk -F, '{print $1}')
+  consumo=$(echo $line | awk -F, '{print $4}')
+
+  if [ "$ciudad" == "$nombre_ciudad" ]; then
+    suma=$((consumo  + suma))
+  fi
+done < consumo.txt
+
+echo "El consumo total de $ciudad es: $suma"
